57abd2944fb6f3da58ebdf4d14fcf401fba7659e
MF.Y <1428483061@qq.com>
2025-08-31 03:30:48 +0800

    更新版权声明

--- Core.py
+++ Core.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2025 Y.MF. All rights reserved.
+#
+# 本代码及相关文档受著作权法保护，未经授权，禁止任何形式的复制、分发、修改或商业使用。
+# 如需使用或修改本代码，请联系版权所有者获得书面许可（联系方式：1428483061@qq.com）。
+#
+# 免责声明：本代码按"原样"提供，不提供任何明示或暗示的担保，包括但不限于对适销性、特定用途适用性的担保。
+# 在任何情况下，版权所有者不对因使用本代码或本代码的衍生作品而导致的任何直接或间接损失承担责任。
+#
+# 项目名称：Core.py
+# 项目仓库：https://github.com/YiMuFeng/KeyWordNoteBook.git
+# 创建时间：2025/8/16 22:57
+# 版权所有者：Y.MF
+# 联系方式：1428483061@qq.com
+# 许可协议：Apache License 2.0
+
 from cryptography.fernet import Fernet
 import json
 import base64
